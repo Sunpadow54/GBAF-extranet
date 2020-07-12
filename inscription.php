96775629085491006fd35456ba1fad1c7d8d0f64
Sunpadow <elsa.dessarps@gmail.com>
2020-07-12 14:41:52 +0200

    Reduction isset tous les champs, avec isset sur submit

--- inscription.php
+++ inscription.php
@@ -20,64 +20,13 @@ catch (Exception $e)
 }
 ?>
 
-<!-- FORMULAIRE D'INSCRIPTION -->
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title> GBAF extranet</title>
-
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" media="all and (min-device-width : 320px) and (max-device-width : 480px)" href="style-mobile.css">
-        <link rel="stylesheet" media="all and (max-device-width: 1280px ) and (min-device-width: 481px)" href="style-tablette.css">
-
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;900&display=swap" rel="stylesheet">  
-    </head>
-    
-  <body>
-    <main class="inscription-connexion">
-      <fieldset>
-          <legend>Créer un compte :</legend>
-          <img src="../images/GBAF.png" alt="Logo GBAF">
-
-          <form method="post" action="inscription">
-            <p>
-                  <label for="pseudo">Identifiant : </label>
-                  <input type="text" id="pseudo" name="username" size="20">
-
-                  <label for="mp">Mot de passe : </label>
-                  <input type="password" id="mp" name="password" size="20">
-
-                  <label for="nom">Nom : </label>
-                  <input type="text" id="nom" name="nom" size="30" value=""
-                  >
-
-                  <label for="prenom">Prénom : </label>
-                  <input type="text" id="prenom" name="prenom" size="30">
-              
-                  <label for="question">Votre question secrète : </label>
-                  <input type="textarea" id="question" name="question">
-
-                  <label for="reponse">La réponse à votre question : </label>
-                  <input type="textarea" id="reponse" name="reponse">
-
-                  <input class="button-envoyer" type="submit" value="Envoyer">
-                  
-                  <span>Les champs indiqués par une * sont obligatoires</span>
-            </p>
-          </form>
-          <a href="connexion.php">se connecter</a>
 
 <!-- VERIFICATION & AJOUT NOUVEL UTILISATEUR -->
 
 <?php
 
-if (isset($_POST['nom']) && isset($_POST['prenom']) 
-&& isset($_POST['username']) && isset($_POST['password']) 
-&& isset($_POST['question']) && isset($_POST['reponse'])) 
+if (isset($_POST['dataPosted']))
 {
-
   $req = $bdd->prepare('SELECT username FROM account WHERE username = ?');
   $req->execute(array($_POST['username']));
   $dataAccount = $req->fetch();
@@ -116,9 +65,54 @@ if (isset($_POST['nom']) && isset($_POST['prenom'])
     }
   }
 }
-
-
 ?>
+<!-- FORMULAIRE D'INSCRIPTION -->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> GBAF extranet</title>
+
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" media="all and (min-device-width : 320px) and (max-device-width : 480px)" href="style-mobile.css">
+        <link rel="stylesheet" media="all and (max-device-width: 1280px ) and (min-device-width: 481px)" href="style-tablette.css">
+
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;900&display=swap" rel="stylesheet">  
+    </head>
+    
+  <body>
+    <main class="inscription-connexion">
+      <fieldset>
+          <legend>Créer un compte :</legend>
+          <img src="../images/GBAF.png" alt="Logo GBAF">
+
+          <form method="post" action="inscription">
+            <p>
+                  <label for="pseudo">Identifiant : </label>
+                  <input type="text" id="pseudo" name="username" size="20">
+
+                  <label for="mp">Mot de passe : </label>
+                  <input type="password" id="mp" name="password" size="20">
+
+                  <label for="nom">Nom : </label>
+                  <input type="text" id="nom" name="nom" size="30" value="">
+
+                  <label for="prenom">Prénom : </label>
+                  <input type="text" id="prenom" name="prenom" size="30">
+              
+                  <label for="question">Votre question secrète : </label>
+                  <input type="textarea" id="question" name="question">
+
+                  <label for="reponse">La réponse à votre question : </label>
+                  <input type="textarea" id="reponse" name="reponse">
+
+                  <input class="button-envoyer" type="submit" name="dataPosted"value="Envoyer">
+                  
+                  <span>Les champs indiqués par une * sont obligatoires</span>
+            </p>
+          </form>
+          <a href="connexion.php">se connecter</a>
       <fieldset>
     </main>
   </body>

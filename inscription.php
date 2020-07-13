8d6bf953fb9a9be6d063ce388ac2e7000d5a14b5
Sunpadow <elsa.dessarps@gmail.com>
2020-07-13 11:15:56 +0200

    Modification CloseCursor()

--- inscription.php
+++ inscription.php
@@ -1,29 +1,31 @@
 <?php
+
 session_start();
+
 
 /* Base de donnée connexion :*/
 
-try
+try 
 {
-    $bdd = new PDO(
-      'mysql:host=localhost;
-      dbname=gbaf-extranet;
-      charset=utf8',
-      'root',
-      '',
-      array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
-    );
+  $bdd = new PDO(
+    'mysql:host=localhost;
+    dbname=gbaf-extranet;
+    charset=utf8',
+    'root',
+    '',
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+  );
 }
-catch (Exception $e)
+
+catch (Exception $e) 
 {
     die('Erreur : ' . $e->getMessage());
 }
-?>
 
 
-<!-- VERIFICATION & AJOUT NOUVEL UTILISATEUR -->
+//------------VERIFICATION & AJOUT NOUVEL UTILISATEUR
 
-<?php
+
 // Verifie si les variables ont été crées
 if (isset($_POST['dataPosted']))
 {
@@ -31,27 +33,34 @@ if (isset($_POST['dataPosted']))
   $req = $bdd->prepare('SELECT username FROM account WHERE username = ?');
   $req->execute(array($_POST['username']));
   $dataAccount = $req->fetch();
+  $req->closeCursor();
+
   if ($dataAccount)
   {
+
     // Username existe déjà
-    $req->closeCursor();
     $erreur = " Ce nom d'utilisateur existe déjà ";
+
   }
+
   else
   {
+
     // Verification que tous les champs ne sont pas vide
-    if (!empty($_POST['nom']) && !empty($_POST['prenom']) 
-    && !empty($_POST['username']) && !empty($_POST['password']) 
-    && !empty($_POST['question']) && !empty($_POST['reponse'])) 
+    if  (!empty($_POST['nom']) && !empty($_POST['prenom']) 
+        && !empty($_POST['username']) && !empty($_POST['password']) 
+        && !empty($_POST['question']) && !empty($_POST['reponse'])) 
     {
+
       // Verification que le mot de passe contient minimum 1 lettre 1 maj et 1 chiffre
-      if (preg_match("#[a-z][A-Z][0-9][._-]?#", $_POST['password']))
+      if (preg_match( "#[a-z][A-Z][0-9][._-]?#", $_POST['password']))
       {
-        $req->closeCursor();
         // Hashage du mot de passe
         $passwordHashed = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
         // Insère le nouvel Utilisateur dans la BDD
-        $req2 = $bdd->prepare('INSERT INTO account (nom, prenom, username, password, question, reponse) VALUES (:nom, :prenom, :username, :password, :question, :reponse)');
+        $req2 = $bdd->prepare ('INSERT INTO account (nom, prenom, username, password, question, reponse) 
+                                VALUES (:nom, :prenom, :username, :password, :question, :reponse)');
         $req2->execute(array(
           'nom' => htmlspecialchars($_POST['nom']),
           'prenom' => htmlspecialchars($_POST['prenom']),
@@ -59,41 +68,60 @@ if (isset($_POST['dataPosted']))
           'password' => $passwordHashed,
           'question' => htmlspecialchars($_POST['question']),
           'reponse' => htmlspecialchars($_POST['reponse'])
-        ));
+          ));
+
         $req2->closeCursor();
+
+        $_SESSION['username'] = htmlspecialchars($_POST['username']);
+
         header('Location: index.php');
+
       }
+
       else
       {
         $erreur ="le mot de passe doit comprendre au moins une lettre, une majuscule et un chiffre";
       }
     }
+
     else
     {
       $erreur =" Veuillez remplir tous les champs";
     }
   }
 }
+
 ?>
-<!-- FORMULAIRE D'INSCRIPTION -->
+
+
+<!-------------- FORMULAIRE D'INSCRIPTION -->
 
 <!DOCTYPE html>
+
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title> GBAF extranet</title>
 
-        <link rel="stylesheet" href="style.css">
-        <link rel="stylesheet" media="all and (min-device-width : 320px) and (max-device-width : 480px)" href="style-mobile.css">
-        <link rel="stylesheet" media="all and (max-device-width: 1280px ) and (min-device-width: 481px)" href="style-tablette.css">
+  <head>
+  
+    <meta charset="utf-8" />
 
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;900&display=swap" rel="stylesheet">  
-    </head>
+    <title> GBAF extranet</title>
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" media="all and (min-device-width : 320px) and (max-device-width : 480px)" href="style-mobile.css">
+    <link rel="stylesheet" media="all and (max-device-width: 1280px ) and (min-device-width: 481px)" href="style-tablette.css">
+
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;900&display=swap" rel="stylesheet">  
+    
+  </head>
     
   <body>
+
     <main class="inscription-connexion">
+
       <fieldset>
+
           <legend>Créer un compte :</legend>
+          
           <img src="../images/GBAF.png" alt="Logo GBAF">
                     
           <?php // message d'erreur
@@ -102,69 +130,85 @@ if (isset($_POST['dataPosted']))
             echo '<span class="message-erreur">' . $erreur .' </span>'; 
           }
           ?>
+
           <!-- Formulaire avec 'value' préenregistrées -->  
           <form method="post" action="inscription">
+
             <p>
-                  <label for="pseudo">Identifiant : </label>
-                  <input type="text" id="pseudo" name="username" size="20" 
-                    <?php
-                    if (isset($_POST['username']))
-                    {
-                      echo 'value = "' .htmlspecialchars($_POST['username']). '"' ;
-                    }
-                    ?>
-                  >
 
-                  <label for="mp">Mot de passe : </label>
-                  <input type="password" id="mp" name="password" size="20">
+              <label for="pseudo">Identifiant : </label>
+              <input type="text" id="pseudo" name="username" size="20" 
+                <?php
+                if (isset($_POST['username']))
+                {
+                  echo 'value = "' .htmlspecialchars($_POST['username']). '"' ;
+                }
+                ?>
+              >
 
-                  <label for="nom">Nom : </label>
-                  <input type="text" id="nom" name="nom" size="30"
-                    <?php
-                    if (isset($_POST['nom']))
-                    {
-                      echo 'value = "' .htmlspecialchars($_POST['nom']). '"' ;
-                    }
-                    ?>
-                  >
 
-                  <label for="prenom">Prénom : </label>
-                  <input type="text" id="prenom" name="prenom" size="30"
-                    <?php
-                    if (isset($_POST['prenom']))
-                    {
-                      echo 'value = "' .htmlspecialchars($_POST['prenom']). '"' ;
-                    }
-                    ?>
-                  >
+              <label for="mp">Mot de passe : </label>
+              <input type="password" id="mp" name="password" size="20">
+
+
+              <label for="nom">Nom : </label>
+              <input type="text" id="nom" name="nom" size="30"
+                <?php
+                if (isset($_POST['nom']))
+                {
+                  echo 'value = "' .htmlspecialchars($_POST['nom']). '"' ;
+                }
+                ?>
+              >
+
+
+              <label for="prenom">Prénom : </label>
+              <input type="text" id="prenom" name="prenom" size="30"
+                <?php
+                if (isset($_POST['prenom']))
+                {
+                  echo 'value = "' .htmlspecialchars($_POST['prenom']). '"' ;
+                }
+                ?>
+              >
+          
+
+              <label for="question">Votre question secrète : </label>
+              <input type="textarea" id="question" name="question"
+                <?php
+                if (isset($_POST['question']))
+                {
+                  echo 'value = "' .htmlspecialchars($_POST['question']). '"' ;
+                }
+                ?>
+              >
+
+
+              <label for="reponse">La réponse à votre question : </label>
+              <input type="textarea" id="reponse" name="reponse"
+                <?php
+                if (isset($_POST['reponse']))
+                {
+                  echo 'value = "' .htmlspecialchars($_POST['reponse']). '"' ;
+                }
+                ?>
+              >
+
+
+              <input class="button-envoyer" type="submit" name="dataPosted"value="Envoyer">
               
-                  <label for="question">Votre question secrète : </label>
-                  <input type="textarea" id="question" name="question"
-                    <?php
-                    if (isset($_POST['question']))
-                    {
-                      echo 'value = "' .htmlspecialchars($_POST['question']). '"' ;
-                    }
-                    ?>
-                  >
+              <span>Les champs indiqués par une * sont obligatoires</span>
 
-                  <label for="reponse">La réponse à votre question : </label>
-                  <input type="textarea" id="reponse" name="reponse"
-                    <?php
-                    if (isset($_POST['reponse']))
-                    {
-                      echo 'value = "' .htmlspecialchars($_POST['reponse']). '"' ;
-                    }
-                    ?>
-                  >
-
-                  <input class="button-envoyer" type="submit" name="dataPosted"value="Envoyer">
-                  
-                  <span>Les champs indiqués par une * sont obligatoires</span>
             </p>
+
           </form>
-          <a href="connexion.php">se connecter</a>
+
+          <a href="index.php">se connecter</a>
+
       <fieldset>
+
     </main>
+
   </body>
+  
 </html>
